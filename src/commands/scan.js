002b7202738857bdfb902f335685bfc2636d5ae9
Vlad <holldike@gmail.com>
2023-04-05 21:50:32 +0300

    logs

--- src/commands/scan.js
+++ src/commands/scan.js
@@ -8,26 +8,27 @@ import getCliArgument from '../functions/getCliArgument.js';
 const filepath = `${outFileDir}/${getCliArgument('--outfile')}`
 
 async function main() {
-  console.log('[GETTING WORDS]')
-  console.log('')
+  const { log } = console
+  log('[GETTING WORDS]')
+  log('')
   let canditates = getAllWords()
 
   // Remove words with lenght less then 3 symbols
   canditates = canditates.filter(item => item.length > 2)
 
   const duplicates = canditates.filter((item, index) => index !== canditates.indexOf(item));
-  
+
   if (duplicates.length) {
-    console.log('Duplicates found:', duplicates)
-    console.log('')
+    log('Duplicates found:', duplicates)
+    log('')
   }
-  
+
   // Remove duplicates
   canditates = [...new Set(canditates)]
 
   const totalCanditates = canditates.length
-  console.log('Total words:', totalCanditates)
-  console.log('')
+  log('Total words:', totalCanditates)
+  log('')
 
   const result = []
   for (const canditate of canditates) {
@@ -35,13 +36,13 @@ async function main() {
       const ens = await lookupENS(canditate)
       result.push(ens)
 
-      console.log(`[${canditates.length}/${result.length} HANDLED]`, ens)
+      log(`[${canditates.length}/${result.length} HANDLED]`, ens)
     } catch (e) {
-      console.log(e)
+      log(e)
     }
   }
 
-  console.log('[WRITING FILE]')
+  log('[WRITING FILE]')
   writeItems(filepath, result)
 }
 
